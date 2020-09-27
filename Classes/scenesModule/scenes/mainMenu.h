4f3c1e8ad5633e6074ada40b191dca7fcc7852e0
Artur Barilo <faustkeller45@gmail.com>
2020-09-27 20:07:46 +0300

    master | wip: scene manager

---
+++ Classes/scenesModule/scenes/mainMenu.h
@@ -0,0 +1,17 @@
+#ifndef MERCENARY_BATTLES_MAINMENU_H
+#define MERCENARY_BATTLES_MAINMENU_H
+
+#include "cocos2d.h"
+#include "coreModule/definitions.h"
+
+namespace mercenaryBattles {
+	using namespace cocos2d;
+	namespace sceneModule {
+		class mainMenu : public Layer {
+		public:
+		};
+	}
+}
+
+
+#endif //MERCENARY_BATTLES_MAINMENU_H
