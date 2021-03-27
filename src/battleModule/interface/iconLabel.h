ae9c5499dbe83c529c3f33096ca276a840a4950f
Artur Barilo <faustkeller45@gmail.com>
2021-03-28 00:08:15 +0900

    master | wip battle module - update icon label widget

--- src/battleModule/interface/iconLabel.h
+++ src/battleModule/interface/iconLabel.h
@@ -1,20 +1,22 @@
 #ifndef MERCENARY_BATTLES_ICONLABEL_H
 #define MERCENARY_BATTLES_ICONLABEL_H
 
-#include "cocos2d.h"
 #include "cocos/ui/CocosGUI.h"
+#include "cocos2d.h"
 #include "common/coreModule/nodes/nodeProperties.h"
 
 namespace mb::battleModule {
 
     class iconLabel
-        : public common::coreModule::nodeProperties, public cocos2d::Sprite {
+        : public common::coreModule::nodeProperties
+        , public cocos2d::Sprite {
       public:
+        enum class eIconLabelTypes { GOLD = 0, TROPHIES };
         iconLabel();
         ~iconLabel() override;
         CREATE_FUNC(iconLabel);
     };
-}
+}// namespace mb::battleModule
 
 
 #endif// MERCENARY_BATTLES_ICONLABEL_H
